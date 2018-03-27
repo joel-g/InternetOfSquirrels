79191e7f019428feefb12e7baf660bd81582be69
joel-g <joelaguerra1@gmail.com>
2018-03-27 16:38:31 -0700

    adds Startup class

---
+++ Squeddit/Squeddit/App_Start/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Globalization;
+
+namespace Squeddit.App_Start
+{
+    public class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+
+        string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant)
+    };
+}
