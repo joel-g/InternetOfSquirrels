66d4b0d71cc2c924e22bf12437801f7cbfb04ca0
joel-g <joelaguerra1@gmail.com>
2018-03-28 09:39:13 -0700

    adds authentication

--- Squeddit/Squeddit/App_Start/Startup.cs
+++ Squeddit/Squeddit/App_Start/Startup.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using Owin;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Configuration;
 using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
 
 namespace Squeddit.App_Start
 {
@@ -14,6 +20,29 @@ namespace Squeddit.App_Start
         private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
         private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
 
-        string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant)
+        string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        AuthenticationFailed = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.Redirect("/Error/message=" + context.Exception.Message);
+                            return Task.FromResult(0);
+                        }
+                    }
+
+                })
+        }
     };
 }
